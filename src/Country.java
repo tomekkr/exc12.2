bfa0a126f586f65f330dc99976b1399a04197481
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-05-18 19:20:23 +0200

    usunąłem niepotrzebne gettery i settery

--- src/Country.java
+++ src/Country.java
@@ -11,30 +11,6 @@ class Country {
         this.population = population;
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(String population) {
-        this.population = population;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
