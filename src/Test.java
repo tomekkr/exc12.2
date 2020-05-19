9e95e22617f4ddb604db12e12bbe04c1c298cdbb
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-05-19 09:31:54 +0200

    - dodałem własny wyjątek NoDataFoundException - dodałem klasę FileService

--- src/Test.java
+++ src/Test.java
@@ -6,34 +6,19 @@ class Test {
     public static void main(String[] args) {
         final String fileName = "countries_data.csv";
         File file = new File(fileName);
-
-        try (
-                Scanner fileScanner = new Scanner(file);
-                Scanner userInputScanner = new Scanner(System.in)
+        try (Scanner fileScanner = new Scanner(file);
+             Scanner userInputScanner = new Scanner(System.in)
         ) {
-            Map<String, Country> countries = new HashMap<>();
 
-            while (fileScanner.hasNextLine()) {
-                String line = fileScanner.nextLine();
-                List<String> elements = List.of(line.split(";"));
-
-                for (int i = 0; i < elements.size(); i++) {
-                    countries.put(elements.get(0), new Country(elements.get(0), elements.get(1), elements.get(2)));
-                }
-            }
-
+            Map<String, Country> countries = FileService.createMapFromFile(fileScanner);
             System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje (np. PL - Polska):");
-            String code = userInputScanner.nextLine();
-            if (countries.containsKey(code)) {
-                Country foundCountry = countries.get(code);
-                System.out.println(foundCountry);
-            } else {
-                System.out.println("Wpisany kod jest nieprawidłowy lub nie ma informacji o takim państwie w pliku: "
-                        + fileName);
-            }
+            String code = FileService.readCodeFromUser(userInputScanner);
+            System.out.println(FileService.getInfoAboutCountry(fileName, countries, code));
 
         } catch (FileNotFoundException e) {
             System.err.println("Nie odnaleziono pliku: " + fileName);
+        } catch (NoDataFoundException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
